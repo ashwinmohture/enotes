a5d1b8ef64c96d5d7556cd34b3cf9d76be6d429b
ashwinmohture <ashu.mohture@gmail.com>
2025-05-25 02:06:53 +0530

    Custom Validation

--- src/main/java/com/enotes/enotes_api/dto/CategoryDto.java
+++ src/main/java/com/enotes/enotes_api/dto/CategoryDto.java
@@ -1,5 +1,9 @@
 package com.enotes.enotes_api.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +17,18 @@ import java.util.Date;
 @NoArgsConstructor
 public class CategoryDto {
     private int id;
+
+    @NotBlank
+    @Min(value = 10, message = "Min 10")
+    @Max(100)
     private String name;
+
+    @NotBlank
+    @Min(value = 10, message = "Min 10")
+    @Max(100)
     private String descriptions;
+
+    @NotNull
     private Boolean isActive;
     private Integer createdBy;
     private Date createdOn;
