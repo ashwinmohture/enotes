0215c6332ba84d5811ccd955c80512d5adf82162
ashwinmohture <ashu.mohture@gmail.com>
2025-06-02 21:53:09 +0530

    download-file implementation

--- src/main/java/com/enotes/enotes_api/service/NotesService.java
+++ src/main/java/com/enotes/enotes_api/service/NotesService.java
@@ -1,6 +1,7 @@
 package com.enotes.enotes_api.service;
 
 import com.enotes.enotes_api.dto.NotesDto;
+import com.enotes.enotes_api.entity.FileDetails;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -10,4 +11,7 @@ public interface NotesService {
     public Boolean saveNotes(String notes, MultipartFile file) throws Exception;
     public List<NotesDto> getAllNotes();
 
+    byte[] downloadFile(FileDetails fileDetails) throws Exception;
+
+    FileDetails getFileDetails(Integer id) throws Exception;
 }
